0f53cf1f9083516b0c5bba7116193d295158c8d7
Kevin Jonson <kcjonson@gmail.com>
2015-06-04 14:05:46 -0700

    Using eventsource for initial model population

--- scripts/apps/dashboard/models/App.js
+++ scripts/apps/dashboard/models/App.js
@@ -56,39 +56,39 @@ define([
 		fetch: function(args) {
 
 
-			var modelsLoaded = 0;
-			var modelsToLoad = Object.keys(MODEL_CONSTRUCTORS).length;
-			if (!!window.EventSource && !this._eventSource) {
-				this._eventSource = new EventSource(SERVER + 'api');
+			// var modelsLoaded = 0;
+			// var modelsToLoad = Object.keys(MODEL_CONSTRUCTORS).length;
+			// if (!!window.EventSource && !this._eventSource) {
+			// 	this._eventSource = new EventSource(SERVER + 'api');
 
-				this._eventSource.addEventListener('modeldata', function(e) {
-					//console.log('Model Data', e.data);
-					var data = JSON.parse(e.data);
-					var model = ENDPOINT_TO_MODEL_MAP[SERVER + data.endpoint];
-					model.set(data.payload);
-					modelsLoaded += 1;
-					if (modelsLoaded == modelsToLoad) {
-						this.trigger('sync:all');
-						this._eventSource.close();
-					}
-				}.bind(this), false);
+			// 	this._eventSource.addEventListener('modeldata', function(e) {
+			// 		//console.log('Model Data', e.data);
+			// 		var data = JSON.parse(e.data);
+			// 		var model = ENDPOINT_TO_MODEL_MAP[SERVER + data.endpoint];
+			// 		model.set(data.payload);
+			// 		modelsLoaded += 1;
+			// 		if (modelsLoaded == modelsToLoad) {
+			// 			this.trigger('sync:all');
+			// 			this._eventSource.close();
+			// 		}
+			// 	}.bind(this), false);
 
-				this._eventSource.addEventListener('error', function(e) {
-					this._eventSource.close();
-				}.bind(this), false);
-			};
+			// 	this._eventSource.addEventListener('error', function(e) {
+			// 		this._eventSource.close();
+			// 	}.bind(this), false);
+			// };
 
 
-			// $.when(
-			// 	this.alarmsModel.fetch(args),
-			// 	this.devicesModel.fetch(args),
-			// 	this.usersModel.fetch(args),
-			// 	this.actionsModel.fetch(args),
-			// 	this.settingsModel.fetch(args),
-			// 	this.weatherModel.fetch(args)
-			// ).done(function(){
-			// 	this.trigger('sync:all');
-			// }.bind(this))
+			$.when(
+				this.alarmsModel.fetch(args),
+				this.devicesModel.fetch(args),
+				this.usersModel.fetch(args),
+				this.actionsModel.fetch(args),
+				this.settingsModel.fetch(args),
+				this.weatherModel.fetch(args)
+			).done(function(){
+				this.trigger('sync:all');
+			}.bind(this))
 		},
 
 		_onModelAll: function(eventName) {
